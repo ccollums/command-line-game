ebeb5e61a7b686563f5d2f80a901105611e17837
Carly Collums <carlycollums@gmail.com>
2021-10-14 14:48:14 -0600

    Refactor game test

--- test/Game-test.js
+++ test/Game-test.js
@@ -17,9 +17,9 @@ describe('Game', function() {
   let game;
 
   beforeEach(function() {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     deck = new Deck(cards);
     round = new Round(deck);
     game = new Game(round)
@@ -33,15 +33,8 @@ describe('Game', function() {
     expect(game).to.be.an.instanceOf(Game);
   })
 
-  it('should keep track of the current round', function() {
-    expect(game.currentRound).to.be.an.instanceOf(Round);
-  })
-
   it('should have a start method to begin the game', function() {
     expect(game.start(card1, deck, round)).to.equal(game.start(card1, deck, round));
   })
 
-  // it.skip('should be able to make a new card in the start method', function() {
-  //   expect(game.start(card1, deck, round)).to.include.an.instanceOf(Card);
-  // })
 })
