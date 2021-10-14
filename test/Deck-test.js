d4eb5a1eabd6e1d9d32382dd1a44f464cb17fabc
Carly Collums <carlycollums@gmail.com>
2021-10-14 14:39:25 -0600

    Refactor Deck test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,31 +6,30 @@ const Card = require('../src/Card.js');
 
 describe('Deck', function() {
 
+  let deck;
+  let card1;
+  let card2;
+  let card3;
+  beforeEach(function() {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck = new Deck([card1, card2, card3]);
+  })
+
   it('should be a function', function() {
-    const deck = new Deck();
     expect(Deck).to.be.a('function');
   });
 
   it('should be an instance of Deck', function() {
-    const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
 
   it('should store the instances of card', function() {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    // const cards = [card1, card2, card3];
-    const deck = new Deck([card1, card2, card3]);
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   });
 
   it('should be able to count the number of cards in the deck', function() {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    // const cards = [card1, card2, card3];
-    const deck = new Deck([card1, card2, card3]);
     deck.countCards();
     expect(deck.countCards()).to.equal(3);
   });
